2888f4a6e0a7f308a324479fea68f9610cfd10c6
Matt Gadda <mgadda@gmail.com>
2011-09-13 22:13:02 -0700

    Initial Commit

---
+++ alisp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  alisp
+//
+//  Created by Matt Gadda on 9/13/11.
+//  Copyright (c) 2011 Catalpa Labs. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
